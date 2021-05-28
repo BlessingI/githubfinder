1d316f7704e8822c0eecb20366977a6196123551
blessing <bigbadumhe@gmail.com>
2021-05-28 09:01:42 -0500

    deploy

---
+++ src/components/layout/Alert.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Alert = ({ alert }) => {
+  return (
+    alert !== null && (
+      <div>
+        <i className ='fas fa-info-circle' /> {alert.msg}
+      </div>
+    )
+
+  )
+}
+
+
+export default Alert
